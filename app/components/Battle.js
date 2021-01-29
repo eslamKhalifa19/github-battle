17cec7ada31efc259d8d927330bc5583067ef76a
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-01-29 08:06:00 +0200

    reset players button

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -142,7 +142,19 @@ export default class Battle extends React.Component {
   render() {
     const { playerOne, playerTwo, battle } = this.state;
     if (battle === true) {
-      return <Results playerOne={playerOne} playerTwo={playerTwo} />;
+      return (
+        <Results
+          playerOne={playerOne}
+          playerTwo={playerTwo}
+          onReset={() =>
+            this.setState({
+              playerOne: null,
+              playerTwo: null,
+              battle: false,
+            })
+          }
+        />
+      );
     }
     return (
       <React.Fragment>
--- app/components/Results.js
+++ app/components/Results.js
@@ -86,27 +86,38 @@ export default class Results extends React.Component {
     }
 
     return (
-      <div className="grid space-around container-sm">
-        <Card
-          header={winner.score === loser.score ? "Tie" : "Winner"}
-          subheader={`Score: ${winner.score.toLocaleString()}`}
-          avatar={winner.profile.avatar_url}
-          href={winner.profile.html_url}
-          name={winner.profile.name}
-        >
-          <ProfileList profile={winner.profile} />
-        </Card>
+      <>
+        <div className="grid space-around container-sm">
+          <Card
+            header={winner.score === loser.score ? "Tie" : "Winner"}
+            subheader={`Score: ${winner.score.toLocaleString()}`}
+            avatar={winner.profile.avatar_url}
+            href={winner.profile.html_url}
+            name={winner.profile.name}
+          >
+            <ProfileList profile={winner.profile} />
+          </Card>
 
-        <Card
-          header={winner.score === loser.score ? "Tie" : "Loser"}
-          subheader={`Score: ${loser.score.toLocaleString()}`}
-          avatar={loser.profile.avatar_url}
-          href={loser.profile.html_url}
-          name={loser.profile.name}
-        >
-          <ProfileList profile={loser.profile} />
-        </Card>
-      </div>
+          <Card
+            header={winner.score === loser.score ? "Tie" : "Loser"}
+            subheader={`Score: ${loser.score.toLocaleString()}`}
+            avatar={loser.profile.avatar_url}
+            href={loser.profile.html_url}
+            name={loser.profile.name}
+          >
+            <ProfileList profile={loser.profile} />
+          </Card>
+        </div>
+        <button onClick={this.props.onReset} className="btn dark-btn btn-space">
+          Reset
+        </button>
+      </>
     );
   }
 }
+
+Results.propTypes = {
+  playerOne: PropTypes.string.isRequired,
+  playerTwo: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired,
+};
