92833bab190b41c00b28bbc4bdb9ab643d1bd009
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-02-09 08:52:48 +0200

    404

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ import Results from "./components/Results";
 import "./index.css";
 import { ThemeProvider } from "./contexts/theme";
 import Nav from "./components/Nav";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -28,9 +28,12 @@ class App extends React.Component {
           <div className={this.state.theme}>
             <div className="container">
               <Nav />
-              <Route exact path="/" component={Popular} />
-              <Route exact path="/battle" component={Battle} />
-              <Route path="/battle/results" component={Results} />
+              <Switch>
+                <Route exact path="/" component={Popular} />
+                <Route exact path="/battle" component={Battle} />
+                <Route path="/battle/results" component={Results} />
+                <Route render={() => <h1>404</h1>} />
+              </Switch>
             </div>
           </div>
         </ThemeProvider>
