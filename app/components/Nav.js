27f31bacc3bcd4ee6a3ae8ea7f27ced2e35d949b
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-02-09 08:05:44 +0200

    navbar

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,11 +1,37 @@
 import React from "react";
 import { ThemeConsumer } from "../contexts/theme";
+import { NavLink } from "react-router-dom";
+
+const activeStyle = {
+  color: "rgb(187,46,31)",
+};
 
 const Nav = () => {
   return (
     <ThemeConsumer>
       {({ theme, toggleTheme }) => (
         <nav className="row space-between">
+          <ul className="row nav">
+            <li>
+              <NavLink
+                exact
+                activeStyle={activeStyle}
+                to="/"
+                className="nav-link"
+              >
+                Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                activeStyle={activeStyle}
+                to="/battle"
+                className="nav-link"
+              >
+                Battle
+              </NavLink>
+            </li>
+          </ul>
           <button
             style={{ fontSize: 30 }}
             className="btn-clear"
