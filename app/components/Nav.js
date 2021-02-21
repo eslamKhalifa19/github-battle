028c53741127ca6831dde1704a305777c186482b
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-02-21 16:29:55 +0200

    refactor card & nav to hooks

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ThemeConsumer } from "../contexts/theme";
+import ThemeContext from "../contexts/theme";
 import { NavLink } from "react-router-dom";
 
 const activeStyle = {
@@ -7,41 +7,29 @@ const activeStyle = {
 };
 
 const Nav = ({ toggleTheme }) => {
+  const theme = React.useContext(ThemeContext);
   return (
-    <ThemeConsumer>
-      {(theme) => (
-        <nav className="row space-between">
-          <ul className="row nav">
-            <li>
-              <NavLink
-                exact
-                activeStyle={activeStyle}
-                to="/"
-                className="nav-link"
-              >
-                Popular
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                activeStyle={activeStyle}
-                to="/battle"
-                className="nav-link"
-              >
-                Battle
-              </NavLink>
-            </li>
-          </ul>
-          <button
-            style={{ fontSize: 30 }}
-            className="btn-clear"
-            onClick={toggleTheme}
-          >
-            {theme === "light" ? "🔦" : "💡"}
-          </button>
-        </nav>
-      )}
-    </ThemeConsumer>
+    <nav className="row space-between">
+      <ul className="row nav">
+        <li>
+          <NavLink exact activeStyle={activeStyle} to="/" className="nav-link">
+            Popular
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeStyle={activeStyle} to="/battle" className="nav-link">
+            Battle
+          </NavLink>
+        </li>
+      </ul>
+      <button
+        style={{ fontSize: 30 }}
+        className="btn-clear"
+        onClick={toggleTheme}
+      >
+        {theme === "light" ? "🔦" : "💡"}
+      </button>
+    </nav>
   );
 };
 
