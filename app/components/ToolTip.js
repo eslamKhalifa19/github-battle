1390864a274c4503e9f17ca3b5152f2dd4bfddb6
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-02-22 07:53:46 +0200

    useHover hook replace hover

--- app/components/ToolTip.js
+++ app/components/ToolTip.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Hover from "./Hover";
-
+import useHover from "../hooks/useHover";
 const styles = {
   container: {
     position: "relative",
@@ -25,15 +24,12 @@ const styles = {
 };
 
 export default function Tooltip({ text, children }) {
+  const [hovering, attrs] = useHover();
   return (
-    <Hover>
-      {(hovering) => (
-        <div style={styles.container}>
-          {hovering === true && <div style={styles.tooltip}>{text}</div>}
-          {children}
-        </div>
-      )}
-    </Hover>
+    <div style={styles.container} {...attrs}>
+      {hovering === true && <div style={styles.tooltip}>{text}</div>}
+      {children}
+    </div>
   );
 }
 Tooltip.propTypes = {
---
+++ app/hooks/useHover.js
@@ -0,0 +1,17 @@
+import { func } from "prop-types";
+import React from "react";
+
+export default function useHover() {
+  const [hovering, setHovering] = React.useState(false);
+
+  const onMouseOver = () => setHovering(true);
+  const onMouseOut = () => setHovering(false);
+
+  return [
+    hovering,
+    {
+      onMouseOut,
+      onMouseOver,
+    },
+  ];
+}
